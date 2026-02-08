a1843f8b50c1bf8501c27bf0952f7b294441a5ed
VPrabhakarN <vijaynagane19@gmail.com>
2026-02-08 20:55:19 +0530

    Got solution for Question 4

--- Solution_4.py
+++ Solution_4.py
@@ -1,10 +1,20 @@
-# Question : Write a program to print 'Hollow Rectangle Star Pattern'.
+# Question : Write a program to print 'Rectangle Star Pattern.
 
 """
 Docstring for Top-100-Pattern-Printing-Programs.Solution_3
                 *****
-                 *****
-                  *****
-                   *****
-
+                *****
+                *****
+                *****
+                *****
 """
+
+# Taking input from the user 
+length_ = int(input("Enter the length : "))
+Width_ = int(input("Enter the width : "))
+
+# Using nested for loop to print 'Ractangle star Pattern"
+for i in range(0, Width_) :
+    for i in range(0, length_) :
+        print("*", end="")
+    print()

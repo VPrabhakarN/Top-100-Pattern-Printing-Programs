7f4c07e198ed40c8ace5e28f1dbfdd3472f8c880
VPrabhakarN <vijaynagane19@gmail.com>
2026-02-09 22:16:37 +0530

    Got 5th question to solve

---
+++ Solution_5.py
@@ -0,0 +1,16 @@
+# Question : Write a program to print 'Hollow Rectangle Star Pattern'.
+"""
+Docstring for Top-100-Pattern-Printing-Programs.Solution_5
+                    *****
+                    *   *
+                    *   *
+                    *   *
+                    *   *
+                    *****
+"""
+
+# Taking an input from the user 
+width = int(input("Enter the width : "))
+height = int(input("Enter the height : "))
+
+# Using for loop to 

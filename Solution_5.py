f5a4079d210d86c926f72a38483e2b804460e5fa
VPrabhakarN <vijaynagane19@gmail.com>
2026-02-09 22:21:18 +0530

    Solved Question 5 | Day 5

--- Solution_5.py
+++ Solution_5.py
@@ -13,4 +13,11 @@ Docstring for Top-100-Pattern-Printing-Programs.Solution_5
 width = int(input("Enter the width : "))
 height = int(input("Enter the height : "))
 
-# Using for loop to 
+# Using for loop to print 'Hollow Rectangle Star Pattern'.
+for i in range(0, width) :
+    for j in range(0, height) :
+        if i==0 or i == width-1 or j == 0 or j == height-1 :
+            print("*", end="")
+        else :
+            print(" ", end="")
+    print()

b0f549d91f39df673b5ac55a7543ca3afa2556a5
VPrabhakarN <vijaynagane19@gmail.com>
2026-02-12 10:47:31 +0530

    Solved Question 8 on Day 8 -  Day 8 / 100 – Top 100 Pattern Printing Challenge | Python Logic Challenge.

---
+++ Solution_8.py
@@ -0,0 +1,22 @@
+# Question : Write a program to print 'Triangle Star Pattern'
+
+"""
+Docstring for Top-100-Pattern-Printing-Programs.Solution_7
+                *
+                **
+                ***
+                **** 
+"""
+
+# Taking the value from the user 
+rows = int(input("Enter the size of rows : "))
+colomns = int(input("Enter the size of colomns : "))
+
+# Using for loop to print 'Triangle Star Pattern'
+for i in range (0, rows) :
+    for j in range(0, colomns) :
+        if j <= i :
+            print("*", end="")
+        else :
+            print(" ", end="")
+    print()

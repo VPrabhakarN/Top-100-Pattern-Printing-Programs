f5de62569a86c9e2c2b05f319e6eab138d59e835
VPrabhakarN <vijaynagane19@gmail.com>
2026-02-18 18:03:28 +0530

    Got Question - 14 | Day 14

--- Solution_14.py
+++ Solution_14.py
@@ -1,5 +1,10 @@
-# Question : Write a program to print 'Basic Square 1 Pattern'. 
+# Question : Write a program to print 'Basic Square Number Pattern'. 
 
 # Taking the input from the user 
+size = int(input("Enter the size : "))
+number = int(input("Enter the number : "))
 
-    
+for i in range(0, size) :
+    for j in range(0, size) :
+        print(number, end=" ")         
+    print()

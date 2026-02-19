6a2ea6df39ee428e8cc1e79aa1e865bf305ad13b
VPrabhakarN <vijaynagane19@gmail.com>
2026-02-19 06:44:58 +0530

    Comments added

--- Number Problems/Solution_1.py
+++ Number Problems/Solution_1.py
@@ -1,5 +1,14 @@
 # Question : Write a program to print 'Basic Square Number Pattern'. 
 
+"""
+Docstring for Top-100-Pattern-Printing-Programs.Number Problems.Solution_1
+                1 1 1 1 1
+                1 1 1 1 1
+                1 1 1 1 1
+                1 1 1 1 1 
+                1 1 1 1 1 
+"""
+
 # Taking the input from the user 
 size = int(input("Enter the size : "))
 number = int(input("Enter the number : "))

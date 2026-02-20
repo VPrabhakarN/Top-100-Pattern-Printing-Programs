cb2eb6d67ee8186aa38d6f281bb0d20cd1e01149
VPrabhakarN <vijaynagane19@gmail.com>
2026-02-20 20:27:48 +0530

    Added comments

--- Number Problems/Solution_16.py
+++ Number Problems/Solution_16.py
@@ -1,5 +1,13 @@
 # Question : Write a program to print 'Basic Right Triangle Number Pattern'.
 
+"""
+1
+2 3
+4 5 6
+7 8 9 10
+11 12 13 14
+"""
+
 # Taking an input from the user
 size = int(input("Enter the size : "))
 
@@ -11,6 +19,6 @@ k = 0
 for i in range(1, size+1) :
     for j in range(0, i) : 
         k += 1
-        print(k, end="")
+        print(k, end=" ")
         
     print()

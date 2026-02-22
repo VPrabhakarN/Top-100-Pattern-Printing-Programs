cd4d47e22742d04cda215c556b302753fe916af6
VPrabhakarN <vijaynagane19@gmail.com>
2026-02-22 16:36:55 +0530

    Solved Question 19 | Day 19

---
+++ Number Problems/Solution_19.py
@@ -0,0 +1,16 @@
+# Question : Write a program to print 'Basic incrementing Triangle Pattern initialized = 3'.
+
+# Taking an input from the user 
+size = int(input("Enter the size : "))
+
+num = size + 2
+
+# Using for loop to print 'Basic incrementing Triangle Pattern initialized = 3'.
+for i in range(size, 0, -1) :
+    for j in range(i, 0, -1) :
+        print(num, end=" ")
+        
+    if num >= 3 :
+        num -= 1
+    print()
+    

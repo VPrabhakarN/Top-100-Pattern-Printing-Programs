ee24fdffc5d48a77bac1553998378d5ab41b7969
VPrabhakarN <vijaynagane19@gmail.com>
2026-02-23 17:23:13 +0530

    Solved Question 19 on Day 19 - Day 19 / 100 – Top 100 Pattern Printing Challenge | Python Logic Challenge.

--- Number Problems/Solution_19.py
+++ Number Problems/Solution_19.py
@@ -1,2 +1,22 @@
 # Question : Write a program to print 'Basic incrementing Triangle Pattern initialised = 3'(Inverted).
 
+"""
+3
+44
+555
+6666
+77777
+
+"""
+
+# Taking an input from the user
+size = int(input("Enter the size : ")) # Number of rows
+
+num = 3 # Initialization = 3
+
+for i in range(0, size) :
+    for j in range(0, i+1) :
+        print(num, end=" ")
+    num += 1
+        
+    print()
